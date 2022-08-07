a310d440c642de84921ec222ed8d0b9ec5fa3b1d
Javier Zamorano <zamorano.javier.m@gmail.com>
2022-08-07 10:52:35 -0400

    integración java

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function(){
+
+    $(".btn").click(function(){
+        $(this).on(alert("El correo fue enviado correctamente"));
+    })
+            
+        $('.btn').tooltip();
+
+
+     $(".fa-solid").click(function(){
+            $(this).css("background-color", "#c52549");
+        })
+
+ 
+
+
+
+})
